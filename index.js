280b1a974b4b33a8847ad4ecb89acaafb152d37a
Will Cooley <william.cooley@gmail.coom>
2023-07-21 17:04:33 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(msg = 'go to the office') {
+    return `This Monday, I will ${msg}.`;
+}
+
+function wrapAdjective(outerInput = "*") {
+    function innerFunction(innerInput = "special") {
+        return `You are ${outerInput}${innerInput}${outerInput}!`
+    }
+    return innerFunction;
+}
